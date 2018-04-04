7efc67c616a1ff0c67a5101ddc181d8ab39af2ba
Ryan Henness <ryanhenness@gmail.com>
2018-04-04 13:01:56 -0700

    Updated with yahoo historicla data

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var app = express();
 
+var yahooFinance = require('yahoo-finance');
+
 app.set('port', process.env.PORT || 5000);
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.send('Hello, World!');
+  yahooFinance.historical({ // HISTORICAL
+    symbol: 'AAPL',
+    from: '2012-01-01',
+    to: '2012-12-31',
+    // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
+  }, function (err, quotes) {
+    res.render("home", { quotes: quotes });
+  });
 });
 
 app.listen(app.get('port'), function() {
   console.log('App is running on port', app.get('port') + "!");
 });
+ 
