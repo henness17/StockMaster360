c0321b24e75611a79434917990e7bb0ebe719e0c
Ryan Henness <ryanhenness@gmail.com>
2018-04-05 11:57:00 -0700

    Added search box, changed url query to '?ticker=[ticker]'

--- index.js
+++ index.js
@@ -8,16 +8,24 @@ app.set('port', process.env.PORT || 5000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function (req, res) {
-  var sym = req.query.symbol;
+  var ticker = req.query.ticker;
   yahooFinance.historical({ // HISTORICAL
-    symbol: sym,
+    symbol: ticker,
     from: '2012-01-01',
     to: '2012-12-31',
     // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
   }, function (err, quotes) {
-    res.render("home", { quotes: quotes, sym: sym });
+    res.render("home", { quotes: quotes, ticker: ticker });
   });
+});
+
+app.post('/search', function (req, res) {
+  var ticker = req.body.ticker;
+  res.redirect('/?ticker=' + ticker);
 });
 
 app.listen(app.get('port'), function() {
