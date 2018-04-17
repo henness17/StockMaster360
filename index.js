7a502a2e469be16b6bd7fd65dae9b4b1cf58bd86
Ryan Henness <ryanhenness@gmail.com>
2018-04-17 10:08:10 -0700

    Added minimal chat

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var yahooFinance = require('yahoo-finance');
 
@@ -31,7 +33,20 @@ app.post('/search', function (req, res) {
   res.redirect('/?ticker=' + ticker);
 });
 
-app.listen(app.get('port'), function() {
-  console.log('App is running on port', app.get('port') + "!");
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
- 
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
