9d54eacd06757e9716cef34b3fd96e0af93250c1
Ryan Henness <ryanhenness@gmail.com>
2018-04-17 12:35:37 -0700

    Beautified

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', function (req, res) {
   var ticker = req.query.ticker;
-  if(req.query.ticker == undefined){
+  if (req.query.ticker == undefined) {
     ticker = 'AMZN';
   }
   yahooFinance.historical({ // HISTORICAL
@@ -24,7 +26,10 @@ app.get('/', function (req, res) {
     to: '2012-12-31',
     // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
   }, function (err, quotes) {
-    res.render("home", { quotes: quotes, ticker: ticker });
+    res.render("home", {
+      quotes: quotes,
+      ticker: ticker
+    });
   });
 });
 
@@ -33,20 +38,20 @@ app.post('/search', function (req, res) {
   res.redirect('/?ticker=' + ticker);
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   console.log('a user connected');
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function () {
     console.log('user disconnected');
   });
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
     console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
 });
 
-http.listen(app.get('port'), function(){
+http.listen(app.get('port'), function () {
   console.log('listening on *:5000');
 });
