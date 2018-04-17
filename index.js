443ca54dc6679d31bdb542eb6c00718152bb88c7
Ryan Henness <ryanhenness@gmail.com>
2018-04-17 10:14:37 -0700

    Fixed port #

--- index.js
+++ index.js
@@ -47,6 +47,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(app.get('port'), function(){
+  console.log('listening on *:5000');
 });
